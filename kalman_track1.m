d319e57de80c322225f0b3468b4aa8db1494c360
ruwan2 <rjw_98@hotmail.com>
2015-12-05 23:25:44 -0500

    begin km track.m 3rd update

--- kalman_track1.m
+++ kalman_track1.m
@@ -22,26 +22,28 @@ matrix_R = sigma_z^2;
 
 n1 = normrnd(0,1);
 hat_x_k1_k1 = hat_x_00;
-a_k = 0.1;
+% a_k = 0.1;
 
 % noise_size=[length, 1];
 noise_size=1;
 % w_k = normrnd(0*noise_size,  matrix_Q*noise_size, [length, 1]);     
-w_k = normrnd(0*noise_size,  matrix_Q*noise_size, [length, 1]);     
-v_k = normrnd(0,  matrix_R, [length, 1]);
+% w_k = normrnd(0*noise_size,  matrix_Q*noise_size, [length, 1]);     
+% v_k = normrnd(0,  matrix_R, [length, 1]);
 hat_P_k1_k1 = matrix_P;
+figure(1);
+x_k=0;
+hold off;
 for iterator=1:length
-    %% Predict
-    hat_x_k_k1 = matrix_F * hat_x_k1_k1 + matrix_G * a_k;
-    hat_P_k_k1 = matrix_F * hat_P_k1_k1 * matrix_F.' + matrix_Q;
-
-
-    %% Update
-    tide_y = z_k - matrix_H * hat_x_k_k1;
-    S_k    = matrix_H * hat_P_k_k1 * matrix_H.' + matrix_R;
-    K_k    = hat_P_k_k1 * matrix_H.' / S_k;
-    hat_x_k1_k1 = hat_x_k_k1 + K_k * tide_y;
-    hat_P_k1_k1 = (eye(2) - K_k * matrix_H) * hat_P_k_k1;
+    w_k = normrnd(0,  sigma_a_sq);     
+    v_k = normrnd(0,  sigma_z_sq);
+    x_k = matrix_F * x_k + w_k;
+    z_k = matrix_H * x_k + v_k;
+    subplot(211);
+    hold on;
+    plot(iterator, x_k(1,1), '*');
+    subplot(212);
+    hold on;
+    plot(iterator, x_k(2,2), '*');
 end
 hat_x_k_k1
 
