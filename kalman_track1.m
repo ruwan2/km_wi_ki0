2d09f80f33318b8504f5918916db9e56c061e66d
ruwan2 <rjw_98@hotmail.com>
2015-12-06 10:31:16 -0500

    km track1.m Successfully get the first estimation, and see the transient process. It is important to know it really is a Klm

--- kalman_track1.m
+++ kalman_track1.m
@@ -8,16 +8,15 @@ matrix_F=[1, delta_t; ...
           0, 1];
 matrix_G=[delta_t^2/2; ...
           delta_t];
-sigma_a_sq = 0.1;      
-sigma_z_sq = 0.12;      
+sigma_a_sq = 1.21;      
+sigma_z_sq = 1.12;      
 matrix_Q=[delta_t^4/4, delta_t^3/2; ...
           delta_t^3/2, delta_t^2]*sigma_a_sq;
-% w_k = normrnd(0,  matrix_Q);     
 matrix_H=[1, 0];
 
-hat_x_00=zeros(2,1);
+% hat_x_00=zeros(2,1);
+hat_x_00=[200;1];
 matrix_P=zeros(2,2);
-% x_k = hat_x_00;
 sigma_z = 0.01;
 matrix_R = sigma_z^2;
 
@@ -25,11 +24,8 @@ n1 = normrnd(0,1);
 hat_x_k1_k1 = hat_x_00;
 a_k = 0.1;
 x_k = [0; 0];
-% noise_size=[length, 1];
-noise_size=1;
-% w_k = normrnd(0*noise_size,  matrix_Q*noise_size, [length, 1]);     
+% noise_size=1;
 figure(2);
-x_k=[0; 0];
 hold off;
 hat_P_k1_k1 = matrix_P;
 for iterator=1:length
@@ -51,8 +47,12 @@ figure(2);
     hat_x_k_k1 = matrix_F * hat_x_k1_k1; % + matrix_G * a_k;
     hat_P_k_k1 = matrix_F * hat_P_k1_k1 * matrix_F.' + matrix_Q;
 figure(3);
+    subplot(311);
     hold on;
     plot(iterator, hat_x_k_k1, '.');
+    subplot(312);
+    hold on;
+    plot(iterator, x_k(1,1)-hat_x_k_k1(1,1), 'r.');
 
 
     %% Update
